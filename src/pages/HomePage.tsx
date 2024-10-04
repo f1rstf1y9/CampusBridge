4302a48d38e9dc07e5b82eac3faf6f3e8184a4ef
f1rstf1y9 <f1rstf1y9@naver.com>
2024-10-04 16:05:42 +0900

    feat: 카메라, 갤러리 버튼 기능 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,18 +8,36 @@ export default function HomePage() {
       <div className="text-center w-8/12 h-full flex flex-col justify-center mx-auto">
         <h1 className="text-4xl font-bold mb-24 text-sky-500">Campus Bridge</h1>
         <div className="flex flex-col gap-4 mb-12">
-          <button
-            className="w-full h-24 bg-sky-300 rounded-lg"
-            onClick={() => navigate("/camera")}
-          >
-            카메라
-          </button>
-          <button
-            onClick={() => navigate("/gallery")}
-            className="w-full h-24 bg-sky-400 rounded-lg"
-          >
-            앨범
-          </button>
+          <div className="w-full h-24 bg-sky-300 rounded-lg">
+            <label
+              htmlFor="camera"
+              className="flex justify-center items-center w-full h-full"
+            >
+              카메라
+            </label>
+            <input
+              id="camera"
+              className="hidden"
+              accept="image/*"
+              type="file"
+              capture="environment"
+            />
+          </div>
+
+          <div className="w-full h-24 bg-sky-400 rounded-lg">
+            <label
+              htmlFor="gallery"
+              className="flex justify-center items-center w-full h-full"
+            >
+              앨범
+            </label>
+            <input
+              id="gallery"
+              className="hidden"
+              accept="image/*"
+              type="file"
+            />
+          </div>
         </div>
       </div>
       <div className="flex w-full h-[50px] justify-center items-center fixed bottom-0 z-10">
