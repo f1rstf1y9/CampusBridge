444d0316e44c628d66f237359caa0206100d7f58
f1rstf1y9 <f1rstf1y9@naver.com>
2024-10-04 17:06:06 +0900

    feat: radix ui 의존성 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <>
-      <div className="bg-slate-100 w-full h-full mx-auto">
+      <div className="bg-slate-100 w-lvw h-lvh mx-auto">
         <Outlet />
       </div>
     </>
