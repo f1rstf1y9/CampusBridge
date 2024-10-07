3d5bdd3c2191775ee2ea2063413f619185c4506a
f1rstf1y9 <f1rstf1y9@naver.com>
2024-10-07 09:45:05 +0900

    feat: 회원가입 페이지

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,5 +1,45 @@
-import React from "react";
+import BackHeader from "@/components/BackHeader";
+import { Flex, TextField, Button } from "@radix-ui/themes";
 
 export default function SignupPage() {
-  return <div>SignupPage</div>;
+  return (
+    <>
+      <BackHeader />
+      <div className="flex w-lvw h-lvh items-center">
+        <form className="w-full" action="#">
+          <Flex direction="column" gap="2" className="w-full items-center">
+            <h1 className="font-bold text-2xl mb-4">회원가입</h1>
+            <TextField.Root
+              placeholder="Nickname"
+              className="w-4/5 h-[50px]"
+              required
+            />
+            <TextField.Root
+              placeholder="ID"
+              className="w-4/5 h-[50px]"
+              required
+            />
+            <TextField.Root
+              placeholder="PASSWORD"
+              className="w-4/5 h-[50px]"
+              type="password"
+              required
+            />
+            <TextField.Root
+              placeholder="CONFIRM PASSWORD"
+              className="w-4/5 h-[50px]"
+              type="password"
+              required
+            />
+            <Button variant="solid" className="w-4/5 h-[50px] mt-4">
+              SIGN UP
+            </Button>
+            <a href="/login" className="underline mt-2 text-slate-400">
+              Login
+            </a>
+          </Flex>
+        </form>
+      </div>
+    </>
+  );
 }
