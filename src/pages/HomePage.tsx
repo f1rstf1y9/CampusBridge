d2051cf8b059239eb21d54a8be6879386d70f6db
f1rstf1y9 <f1rstf1y9@naver.com>
2024-10-07 14:01:23 +0900

    style: 메인페이지 스타일링

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,32 @@
 import Footer from "@/components/Footer";
 
+import MainLogo from "@/assets/images/main-logo.png";
+import IconCamera from "@/assets/icons/icon_camera.svg";
+import IconAlbum from "@/assets/icons/icon_album.svg";
+
 export default function HomePage() {
   return (
     <>
       <div className="text-center w-8/12 h-full flex flex-col justify-center mx-auto">
-        <h1 className="text-4xl font-bold mb-24 text-sky-500">Campus Bridge</h1>
+        <div className="text-4xl font-bold mb-16 text-sky-500">
+          <img src={MainLogo} alt="Campus Bridge" />
+          <span className="text-xl text-sky-400">캠퍼스 브릿지</span>
+        </div>
         <div className="flex flex-col gap-4 mb-12">
-          <div className="w-full h-24 bg-sky-300 rounded-lg">
+          <div className="w-full h-24 bg-white rounded-xl shadow-clay-01">
             <label
               htmlFor="camera"
-              className="flex justify-center items-center w-full h-full"
+              className="flex justify-center items-center w-full h-full gap-4"
             >
-              카메라
+              <img
+                src={IconCamera}
+                alt="camera"
+                width="40px"
+                className="ml-[-40px]"
+              />
+              <span className="text-sky-500 text-xl font-bold ml-[40px]">
+                카메라
+              </span>
             </label>
             <input
               id="camera"
@@ -22,12 +37,20 @@ export default function HomePage() {
             />
           </div>
 
-          <div className="w-full h-24 bg-sky-400 rounded-lg">
+          <div className="w-full h-24 bg-sky-400 rounded-xl shadow-clay-02">
             <label
               htmlFor="gallery"
-              className="flex justify-center items-center w-full h-full"
+              className="flex justify-center items-center w-full h-full gap-4"
             >
-              앨범
+              <img
+                src={IconAlbum}
+                alt="album"
+                width="40px"
+                className="ml-[-40px]"
+              />
+              <span className="text-white text-xl font-bold ml-[40px]">
+                앨범
+              </span>
             </label>
             <input
               id="gallery"
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "clay-01":
+          "15px 15px 40px 0px rgba(43, 154, 232, 0.5), inset -6px -6px 10px 0px rgba(43, 154, 232, 0.4), inset 0px 6px 14px 0px rgb(255, 255, 255)",
+        "clay-02":
+          "15px 15px 40px 0px rgba(43, 154, 232, 0.5), inset -6px -6px 10px 0px rgba(43, 131, 232, 0.4), inset 0px 6px 14px 0px rgb(172, 212, 243)",
+      },
+    },
   },
   plugins: [],
 };
