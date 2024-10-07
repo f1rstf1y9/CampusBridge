373c5c2624000a5665c64704594092c5cf1ad540
f1rstf1y9 <f1rstf1y9@naver.com>
2024-10-07 15:48:27 +0900

    feat: 로딩페이지 이미지 데이터 로딩 실패 에러 핸들링

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,22 +1,40 @@
-import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import ErrorModal from "@/components/ErrorModal";
 
 export default function LoadingPage() {
   const location = useLocation();
+  const navigate = useNavigate();
   const croppedImage = location.state?.croppedImage;
+
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    if (!croppedImage) {
+      setShowModal(true);
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    }
+  }, [croppedImage, navigate]);
 
   return (
     <>
-      <div className="wmx-auto flex justify-center items-center h-lvh">
-        <div className="fixed z-10 text-center">
-          <iframe src="https://lottie.host/embed/a807f7e6-9b39-48ef-9e50-183909786a6a/3dS6Zu16YD.json"></iframe>{" "}
-          <span className="text-white font-bold drop-shadow">번역 중...</span>
+      {showModal && <ErrorModal />}
+      {croppedImage && (
+        <div className="wmx-auto flex justify-center items-center h-lvh">
+          <div className="fixed z-10 text-center">
+            <iframe src="https://lottie.host/embed/a807f7e6-9b39-48ef-9e50-183909786a6a/3dS6Zu16YD.json"></iframe>{" "}
+            <span className="text-white font-bold drop-shadow">번역 중...</span>
+          </div>
+          <img
+            src={croppedImage ? URL.createObjectURL(croppedImage) : ""}
+            width="300px"
+            className="rounded brightness-50 opacity-60"
+          />
         </div>
-        <img
-          src={croppedImage ? URL.createObjectURL(croppedImage) : ""}
-          width="300px"
-          className="rounded brightness-50 opacity-60"
-        />
-      </div>
+      )}
     </>
   );
 }
