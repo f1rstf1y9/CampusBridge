173e47574883bbd4cbeda9ada7a3c7a34177e6f7
f1rstf1y9 <f1rstf1y9@naver.com>
2024-10-07 14:18:27 +0900

    style: 로그인, 회원가입 폼 스타일링

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,19 +9,22 @@ export default function LoginPage() {
       <div className="flex w-lvw h-lvh items-center">
         <form className="w-full" action="#">
           <Flex direction="column" gap="2" className="w-full items-center">
-            <h1 className="font-bold text-2xl mb-4">로그인</h1>
+            <h1 className="font-bold text-2xl mb-4 text-sky-400">로그인</h1>
             <TextField.Root
               placeholder="ID"
-              className="w-4/5 h-[50px]"
+              className="w-4/5 h-[50px] rounded-xl shadow-clay-white-sm"
               required
             />
             <TextField.Root
               placeholder="PASSWORD"
               type="password"
-              className="w-4/5 h-[50px]"
+              className="w-4/5 h-[50px] rounded-xl shadow-clay-white-sm"
               required
             />
-            <Button variant="solid" className="w-4/5 h-[50px] mt-4">
+            <Button
+              variant="solid"
+              className="w-4/5 h-[50px] mt-4 bg-sky-400 rounded-xl shadow-clay-blue-sm"
+            >
               LOGIN
             </Button>
             <Link to="/signup" className="underline mt-2 text-slate-400">
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -9,30 +9,33 @@ export default function SignupPage() {
       <div className="flex w-lvw h-lvh items-center">
         <form className="w-full" action="#">
           <Flex direction="column" gap="2" className="w-full items-center">
-            <h1 className="font-bold text-2xl mb-4">회원가입</h1>
+            <h1 className="font-bold text-2xl mb-4 text-sky-400">회원가입</h1>
             <TextField.Root
               placeholder="Nickname"
-              className="w-4/5 h-[50px]"
+              className="w-4/5 h-[50px] rounded-xl shadow-clay-white-sm"
               required
             />
             <TextField.Root
               placeholder="ID"
-              className="w-4/5 h-[50px]"
+              className="w-4/5 h-[50px] rounded-xl shadow-clay-white-sm"
               required
             />
             <TextField.Root
               placeholder="PASSWORD"
-              className="w-4/5 h-[50px]"
+              className="w-4/5 h-[50px] rounded-xl shadow-clay-white-sm"
               type="password"
               required
             />
             <TextField.Root
               placeholder="CONFIRM PASSWORD"
-              className="w-4/5 h-[50px]"
+              className="w-4/5 h-[50px] rounded-xl shadow-clay-white-sm"
               type="password"
               required
             />
-            <Button variant="solid" className="w-4/5 h-[50px] mt-4">
+            <Button
+              variant="solid"
+              className="w-4/5 h-[50px] mt-4 bg-sky-400 rounded-xl shadow-clay-blue-sm"
+            >
               SIGN UP
             </Button>
             <Link to="/login" className="underline mt-2 text-slate-400">
